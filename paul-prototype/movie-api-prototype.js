205b827e6efbdf1f1ef63c43ccf535134715e57d
paulswann44 <99698080+paulswann44@users.noreply.github.com>
2022-12-07 15:06:11 -0600

    peer programming power

--- paul-prototype/movie-api-prototype.js
+++ paul-prototype/movie-api-prototype.js
@@ -34,29 +34,48 @@ function promiseState(promise, callback) {
     $('#submit-btn').click((event) => {
         event.preventDefault();
         let search = $('#search').val()
-        getMovies(search);
+        getOmdMovies(search);
+        // getGlitchMovies(search);
+
+
     });
 
 
 //+++++++++++++++++++++++++++++++++++++++++++++GET ME +++++++++++++++++++++++++++++++++++++++++++++
+function saveMovies (){
+   let searchResult = [];
 
+}
 
-    const getMovies = (search) => {
+    const getOmdMovies = (search) => {
         console.log(search);
-         let promise = fetch(`https://www.omdbapi.com?apikey=${movieKey}&s=${search}`)
-            // if (promise ===)
+        // alert('getOmdMovies get request works')
+         let promise = fetch(`https://www.omdbapi.com?apikey=${movieKey}&s=${search}&Plot=Short`)
+
             .then(response => response.json())//then... return json
             .then(function (data) { //then return data
                 console.log('get data', data);
 
                 let movie = data.Search;
-                let appendMovies = append(movie)
+                // console.log("getOmdMovies first: ", data.Search[0].Title);
+                for (let i=0;i<data.Search.length;i++){
+
+                    const movieObj = {title: `${data.Search[i].Title.toString() }`, year:data.Search[i].Year.toString() }
+
+                    postMovie(movieObj);
+                }
+                let appendMovies = append(movie) //cards are made
                 $('#append-movies').html(appendMovies)  //I want to take the results and place it in the div w/ ID of movies
             })
             .catch((error) => {
                 console.log(error);
             })
     }
+
+
+
+
+
 
 
     let append = function (data) {
@@ -72,7 +91,6 @@ function promiseState(promise, callback) {
                          <p>Movie Year: ${Year}</p>
                          <p>Movie ID: ${imdbID}</p>
                          <p>Movie type: ${Type}</p>
-                         
                          </div>`;
 
         }
@@ -84,11 +102,10 @@ function promiseState(promise, callback) {
 //+++++++++++++++++++++++++++++++++++++++++++++POST ME+++++++++++++++++++++++++++++++++++++++++++++
 
 function postMovie (movie) {
-    movie.preventDefault()
 //TODO: get the length of database (data)
-//     console.log("hello")
+    console.log(movie)
     // movie.preventDefault()
-    const movieObj = {title: `${movie.target[0].value}`, body: '2'}
+    const movieObj = movie;
     const option =   {
         method: 'POST',
         headers: {
@@ -100,7 +117,7 @@ function postMovie (movie) {
     fetch(url, option)
         .then(response => response.json())
         .then(function (data) {
-            alert('Post successful')
+            // alert('Post successful')
             console.log('data', data);
         })
         .catch((error) => {
@@ -112,46 +129,72 @@ let post= document.getElementById('new-movie')
 post.addEventListener('submit', postMovie)
 
 
+//GET MOVIES IN 'DATABASE'
+//NOT READY-WAITING ON THE FRONT-END TO BE MORE DEVELOP
+const getGlitchMovies = (title, year) => {
+        title = title.toString().toUpperCase();
+    console.log(title, year);
+    fetch(`https://invented-fantastic-sense.glitch.me/movies`)
+        .then(response => response.json())//then... return json
+        .then(function (data) { //then return data
+            console.log('getGlitchMovies: ', data.filter(function(item){
+
+                return item.title.toString().toUpperCase() === title;
+                    // && item.year === search.year;  //DEVELOP MORE HERE
+            }));
+
+
+
+
+            // let movie = data.Search.title;
+            // let appendMovies = append(movie)
+            // $('#append-movies').html(appendMovies)  //I want to take the results and place it in the div w/ ID of movies
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+}
+
 
 //+++++++++++++++++++++++++++++++++++++++++++++PUT ME+++++++++++++++++++++++++++++++++++++++++++++
 
 
 
-// function putMovie (id) {
-//     movie.preventDefault()
-//
-//     console.log("hello")
-//     // movie.preventDefault()
-//     const movieObj = {title: "Hello", body: '3'}
-//     const option =   {
-//         method: 'PUT',
-//         headers: {
-//             'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(movieObj),
-//     };
-//     console.log('put option', option)
-//     fetch(`https://invented-fantastic-sense.glitch.me/movies/${id}`, option)
-//         .then(response => response.json())
-//         .then(function (data) {
-//             alert('movie was good to watch')
-//             console.log('data', data);
-//         })
-//         .catch((error) => {
-//             console.log(error);
-//         })
-//
-// }
-//
-//
-// let inputMovie= document.getElementById('new-movie')
-// inputMovie.addEventListener('submit', putMovie)
+function putMovie (id) {
+    id.preventDefault()
+
+    console.log("hello")
+    // movie.preventDefault()
+    const movieObj = {title: "Hello", body: '3'}
+    const option =   {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(movieObj),
+    };
+    console.log('put option', option)
+    fetch(`https://invented-fantastic-sense.glitch.me/movies/${id}`, option)
+        .then(response => response.json())
+        .then(function (data) {
+            alert('movie was good to watch')
+            console.log('data', data);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+
+}
+
+
+let inputMovie= document.getElementById('new-movie')
+inputMovie.addEventListener('submit', putMovie)
 
 //+++++++++++++++++++++++++++++++++++++++++++++DELETE ME+++++++++++++++++++++++++++++++++++++++++++++
 
         function deleteMovie(search) {
             search.preventDefault()
-            console.log("hello")
+            console.log("delete works!")
             const movieObj = {title: "Hello", body: '1'}
             const url = "https://invented-fantastic-sense.glitch.me/movies/"
             const option = {
